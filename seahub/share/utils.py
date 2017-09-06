d539506db49dc640a15aa116df0cb0bf227de87c
zming <517046497@qq.com>
2017-09-06 11:20:40 +0800

    [API] share to groups, add test

---
+++ seahub/share/utils.py
@@ -0,0 +1,16 @@
+from seahub.group.utils import is_group_member
+from seahub.constants import PERMISSION_ADMIN
+from seahub.share.models import ExtraSharePermission, ExtraGroupsSharePermission
+
+def is_repo_admin(username, repo_id):
+    # If user either is a repo admin or a group that belong to admin rights, user can share it with other groups or groups.
+    is_administrator = ExtraSharePermission.objects.\
+            get_user_permission(repo_id, username) == PERMISSION_ADMIN
+    belong_to_admin_group = False
+    group_ids = ExtraGroupsSharePermission.objects.get_admin_groups(repo_id)
+    for group_id in group_ids:
+        if is_group_member(group_id, username):
+            belong_to_admin_group = True
+            break
+
+    return is_administrator or belong_to_admin_group
